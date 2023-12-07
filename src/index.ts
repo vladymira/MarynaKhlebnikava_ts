818366bafa07fca77440618ec777eba311ffb179
Marina Khlebnikova <khlebnikova@irc.beltelecom.by>
2023-12-07 18:15:04 +0300

    classwork05122023_rework

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
+import { displayLS } from "./displayLS";
 import { record } from "./recordType";
 
-// способы найти форму
- const test1 = document.getElementsByName('test')[0];  // этот способ плох - в ts не подходит для дальнейшего new FormData, djpdhfoftn NodeList 
- const test2 = document.forms.namedItem('test');
-// const test3 = document.forms.test; // тоже не годится для ts
+//////////////// три способа найти форму
+const test1 = document.getElementsByName('test')[0];  // этот способ плох - в ts не подходит для дальнейшего new FormData, возвращает NodeList 
+const test2 = document.forms.namedItem('test');
+ //const test3 = document.forms.test; // тоже не годится для ts(Property 'test' does not exist on type 'HTMLCollectionOf<HTMLFormElement>')
 
-if(test2) 
-{ 
-    const button  = document.getElementsByName('button')[0];
-    
-   
-    button?.addEventListener('click', (event) => {   
-        event.preventDefault();
+
+const data = localStorage.getItem('tasks');
+const listTodo: Array<record> = JSON.parse(data || '{}');
+
+//выводим список из хранилища при первом входе/обновлении страницы
+displayLS('tasks', test2);
   
+
+if (test2) {
+    const button = document.getElementsByName('button')[0];
+
+    button?.addEventListener('click', (event) => {
+        event.preventDefault();
+
         //объект, представляющий данные HTML формы
         const formData = new FormData(test2);
-        const todoText = String(formData.get('todo')).trim();         
-        let listTodo: Array<record> = [];
+        // считываем значение input
+        const todoText = String(formData.get('todo')).trim();
+        
+        //дбавляем задачу в список задач  
+        listTodo.push({ task: todoText, data: (new Date()).toLocaleString() });
 
-        listTodo.push({ task: todoText, data: (new Date()).toLocaleString()});
-
+        // перезаписываем этот список в хранилище
         localStorage.setItem('tasks', JSON.stringify(listTodo));
 
-
-        let p = document.createElement('p');
-	    p.textContent = localStorage.getItem('tasks');
-        test2.appendChild(p);       
-        let resetForm = <HTMLFormElement>document.getElementById('todo');
-        debugger;
-        resetForm.reset();        
-            
-})
+       // выводим список из хранилища
+        displayLS('tasks', test2);
+        
+        // очищаем инпут
+        (document.getElementById('todo') as HTMLInputElement).value = '';          
+    });
 }
 
 
